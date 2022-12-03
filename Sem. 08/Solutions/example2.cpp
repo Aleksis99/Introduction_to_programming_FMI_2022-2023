5e40b95ff052639ca6440d052dc3dea974f1664c
Aleksis99 <49377192+Aleksis99@users.noreply.github.com>
2022-12-03 10:28:07 +0200

    Add files via upload

---
+++ Sem. 08/Solutions/example2.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+void inputMatrix(int**& matrix, int& n, int*& sizes )
+{
+
+	int m;
+
+	cin >> n;
+
+
+	matrix = new int*[n];
+
+	sizes = new int[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> m;
+
+		sizes[i] = m;
+
+		matrix[i] = new int[m];
+
+		for (int j = 0; j < m; j++)
+		{
+			cin >> matrix[i][j];
+		}
+	}
+
+}
+
+int* concat( int* arr1, int n, int* arr2, int m)
+{
+	int* newArr = new int[n + m];
+
+	for (int i = 0; i < n; i++)
+	{
+		newArr[i] = arr1[i];
+	}
+
+	for (int i = 0; i < m; i++)
+	{
+		newArr[i + n] = arr2[i];
+	}
+
+	return newArr;
+}
+
+int** mergeRows( int** matrix, int n, int* sizes)
+{
+	int newN = (int)ceil(n / 2.);
+
+	int** newMatrix = new int* [newN];
+
+	for (int i = 0; i < n / 2; i++)
+	{
+		newMatrix[i] = concat(matrix[i], sizes[i], matrix[n - 1 - i], sizes[n - 1 - i]);
+	}
+
+	if (n % 2 == 1)
+	{
+		newMatrix[newN - 1] = new int[sizes[newN - 1]];
+
+		for (int i = 0; i < sizes[newN - 1]; i++)
+		{
+			newMatrix[newN - 1][i] = matrix[newN - 1][i];
+		}
+	}
+
+	return newMatrix;
+}
+
+int main()
+{
+	int** matrix;
+	int* sizes;
+	int n;
+	inputMatrix(matrix, n, sizes);
+
+	int** newMatrix = mergeRows(matrix, n, sizes);
+
+	for (int i = 0; i < ceil(n/2.); i++)
+	{
+		if (n % 2 == 1 && i == n / 2 + 1) 
+		{
+			for (int j = 0; j < sizes[i]; j++)
+			{
+				cout << newMatrix[i][j] << " ";
+			}
+			cout << endl;
+		}
+		else 
+		{
+			for (int j = 0; j < sizes[i] + sizes[n - 1 - i]; j++)
+			{
+				cout << newMatrix[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+}
---
+++ Sem. 08/Solutions/task1.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+
+using namespace std;
+
+void inputArr(int* arr, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+}
+
+void printArr(const int* arr, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
+
+void printMatrix(const int** arr, int n, int m)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+
+		cout << endl;
+	}
+}
+
+void inputMatrix(int** arr, int n, int m)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+}
+
+void merge(int* arr1, int size1, const int* arr2, int size2)
+{
+	int* resultArr = new int[size1 + size2];
+
+	int index1 = 0;
+	int index2 = 0;
+	int currIndex = 0;
+
+	while (index1 < size1 && index2 < size2)
+	{
+		if (arr1[index1] <= arr2[index2])
+		{
+			resultArr[currIndex++] = arr1[index1++];
+		}
+		else
+		{
+			resultArr[currIndex++] = arr2[index2++];
+		}
+	}
+
+	while (index1 < size1)
+	{
+		resultArr[currIndex++] = arr1[index1++];
+	}
+
+	while (index2 < size2)
+	{
+		resultArr[currIndex++] = arr2[index2++];
+	}
+
+	delete[] arr1;
+
+	arr1 = resultArr;
+}
+
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+
+	int* arrA = new int[n];
+	int* arrB = new int[m];
+
+	inputArr(arrA, n);
+	inputArr(arrB, m);
+
+	merge(arrA, n, arrB, m);
+
+	printArr(arrA, n+m);
+}
